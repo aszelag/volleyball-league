5c3064fab9309f728660fe3d81e8a04a23c7409a
Asia <szelagasia@gmail.com>
2019-08-19 17:05:35 +0200

    Change class Aplication

--- src/main/java/Aplication.java
+++ src/main/java/Aplication.java
@@ -5,9 +5,16 @@ public class Aplication {
         Player player1 = new Player(1, "Jan", "Kowalsi");
         Player player2 = new Player(2, "Adam", "Nowak");
 
+        Player player3 = new Player(1, "Maciej", "Kowalsi");
+        Player player4 = new Player(2, "Adrian", "Nowak");
+
         Team team1 = new Team();
         team1.addPlayer(player1);
         team1.addPlayer(player2);
+
+        Team team2 = new Team();
+        team2.addPlayer(player3);
+        team2.addPlayer(player4);
 
 
 
